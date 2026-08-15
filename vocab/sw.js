50ad18b9dbe40c9e41b09982bc3148a2ba595580
sakamototakurounin-glitch <sakamototakurounin@gmail.com>
2026-08-15 19:51:04 +0900

    Add files via upload

--- vocab/sw.js
+++ vocab/sw.js
@@ -1,19 +1,12 @@
-const CACHE='vocabstar-cache-v4-files-flashcards';
+const CACHE='vocabstar-cache-v5-folder-audio';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))
-});
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
   self.clients.claim()
 ])));
 self.addEventListener('fetch',e=>{
-  e.respondWith(
-    fetch(e.request).then(resp=>{
-      const copy=resp.clone();
-      caches.open(CACHE).then(c=>c.put(e.request,copy));
-      return resp
-    }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))
-  )
+  e.respondWith(fetch(e.request).then(resp=>{
+    const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp
+  }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))
 });

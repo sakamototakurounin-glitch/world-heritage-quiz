126fc22f06a5474e15330581a0aa42ef243c3b1c
sakamototakurounin-glitch <sakamototakurounin@gmail.com>
2026-08-16 14:27:14 +0900

    Add files via upload

--- vocab/sw.js
+++ vocab/sw.js
@@ -1,4 +1,4 @@
-const CACHE='vocabstar-cache-v12-file-star-restored';
+const CACHE='vocabstar-cache-v13-rename-position';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
